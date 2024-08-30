92aebd0f5dd88710f11a37ff438be0ff90f93b8e
Rasika Priyadarshani <rasikap@alliontechnologies.com>
2024-08-30 11:35:47 +0530

    Update react-doc-viewer

--- src/app/react-doc-viewer/page.tsx
+++ src/app/react-doc-viewer/page.tsx
@@ -1,27 +1,41 @@
 "use client";
 
 import React from "react";
-import DocViewer, {
-  DocViewerRenderers,
- 
-} from "@cyntler/react-doc-viewer";
+import DocViewer, { DocViewerRenderers } from "react-doc-viewer";
 
-import "@cyntler/react-doc-viewer/dist/index.css";
+// import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
+// import "@cyntler/react-doc-viewer/dist/index.css";
 
 // https://www.npmjs.com/package/@cyntler/react-doc-viewer
 // https://www.npmjs.com/package/react-doc-viewer
 // https://github.com/Alcumus/react-doc-viewer/issues/72
 const Home = () => {
   const docs = [
-    //   {
-    //   uri: "https://calibre-ebook.com/downloads/demos/demo.docx",
-    //   fileType: 'docx',
-    //   // uri: "/TESTDocument.docx"
-    //   // uri: "/To-do list.xlsx",
-    //   // uri: "/Portfolio.pptx",
-    //   // uri: "/sample.txt"
-    //  // uri: "/Employer-Consent-Letter.pdf"
-    // },
+    {
+      uri: "https://doc-viewer-app.vercel.app/Employer-Consent-Letter.pdf",
+      fileType: "pdf",
+      // uri: "/TESTDocument.docx"
+      // uri: "/To-do list.xlsx",
+      // uri: "/Portfolio.pptx",
+      // uri: "/sample.txt"
+      // uri: "/Employer-Consent-Letter.pdf"
+    },
+    {
+      uri: "https://doc-viewer-app.vercel.app/TESTDocument.docx",
+      fileType: "docx",
+    },
+    {
+      uri: "https://doc-viewer-app.vercel.app/To-do list.xlsx",
+      fileType: "xlsx",
+    },
+    {
+      uri: "https://doc-viewer-app.vercel.app/Portfolio.pptx",
+      fileType: "pptx",
+    },
+    {
+      uri: "https://doc-viewer-app.vercel.app/sample.txt",
+      fileType: "txt",
+    },
     {
       uri: "https://sample-videos.com/xls/Sample-Spreadsheet-10-rows.xls",
       fileType: "xls",
@@ -34,19 +48,12 @@ const Home = () => {
       uri: "https://sample-videos.com/ppt/Sample-PPT-File-500kb.ppt",
       fileType: "ppt",
     },
-
-    {
-      uri: "/Employer-Consent-Letter.pdf", // Direct path from the public directory
-      fileType: "pdf",
-    },
   ];
 
   return (
     <DocViewer
       pluginRenderers={DocViewerRenderers}
-      //pluginRenderers={[PDFRenderer]}
       documents={docs}
-      //style={{ width: 500, height: 500 }}
       className="test-class"
       config={{
         header: {
