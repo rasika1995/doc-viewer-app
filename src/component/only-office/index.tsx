013b1c7b330594fc92b4edfba65acc68247d3014
Rasika Priyadarshani <rasikap@alliontechnologies.com>
2024-10-11 22:21:06 +0530

    Configure View Only Doc View For ONLY OFFICE & Configure different permission for the Doc

--- src/component/only-office/index.tsx
+++ src/component/only-office/index.tsx
@@ -13,6 +13,11 @@ interface DocumentConfig {
   key: string;
   title: string;
   url: string;
+  permissions: {
+    download: boolean;
+    edit?: boolean;
+    print?: boolean;
+  };
 }
 
 const OnlyOffice = () => {
@@ -41,6 +46,11 @@ const OnlyOffice = () => {
       key: "file-sample_100kB6",
       title: "file-sample_100kB3",
       url: "https://doc-viewer-app.vercel.app/file-sample_100kB.doc",
+      permissions: {
+        download: false,
+        edit: false,
+        print: false
+      },
       // url: "http://192.168.8.152:3000/file-sample_100kB.doc",
     },
     {
@@ -48,6 +58,10 @@ const OnlyOffice = () => {
       key: "file-sample_1MB6",
       title: "file-sample_1MB",
       url: "https://doc-viewer-app.vercel.app/file-sample_1MB.docx",
+      permissions: {
+        download: true,
+      },
+
       // url: "http://192.168.8.152:3000/file-sample_1MB.docx",
     },
     {
@@ -55,6 +69,9 @@ const OnlyOffice = () => {
       key: "file-sample_15MB6",
       title: "file-sample_15MB",
       url: "https://doc-viewer-app.vercel.app/15-MB-docx-file-download.docx",
+      permissions: {
+        download: true,
+      },
       // url: "http://192.168.8.152:3000/15-MB-docx-file-download.docx",
     },
     {
@@ -62,6 +79,9 @@ const OnlyOffice = () => {
       key: "AWS Overview Notes4",
       title: "AWS Overview Notes.docx",
       url: "https://doc-viewer-app.vercel.app/AWS Overview Notes.docx",
+      permissions: {
+        download: true,
+      },
       // url: "http://192.168.8.152:3000/AWS Overview Notes.docx",
     },
     {
@@ -69,6 +89,9 @@ const OnlyOffice = () => {
       key: "sample-test-key-gc-1",
       title: "To-do List",
       url: "https://doc-viewer-app.vercel.app/To-do%20list.xlsx",
+      permissions: {
+        download: true,
+      },
       // url: "http://192.168.8.152:3000/To-do%20list.xlsx",
     },
     {
@@ -76,6 +99,9 @@ const OnlyOffice = () => {
       key: "apiwh1e490e07-bcab-469a-801b-5e0af8faa3a27",
       title: "Portfolio Presentation",
       url: "https://doc-viewer-app.vercel.app/Portfolio.pptx",
+      permissions: {
+        download: true,
+      },
       // url: "http://192.168.8.152:3000/Portfolio.pptx",
     },
     {
@@ -83,6 +109,9 @@ const OnlyOffice = () => {
       key: "key-docx-155",
       title: "Test Document",
       url: "https://doc-viewer-app.vercel.app/TESTDocument.docx",
+      permissions: {
+        download: true,
+      },
       // url: "http://192.168.8.152:3000/TESTDocument.docx",
     },
     {
@@ -90,6 +119,9 @@ const OnlyOffice = () => {
       key: "key-pdf",
       title: "Employer Consent Letter",
       url: "https://doc-viewer-app.vercel.app/Employer-Consent-Letter.pdf",
+      permissions: {
+        download: true,
+      },
       // url: "http://192.168.8.152:3000/Employer-Consent-Letter.pdf",
     },
   ];
@@ -135,6 +167,7 @@ const OnlyOffice = () => {
           document: docConfig,
           documentType: documentTypeMap[docConfig.fileType], // Change based on your document type https://api1.onlyoffice.com/editors/config
           editorConfig: {
+            mode: "view",
             // callbackUrl: "http://localhost/editors/callback", // Your callback URL for saving documents
             // customization: {
             //   anonymous: {
@@ -145,6 +178,32 @@ const OnlyOffice = () => {
             //   },
             //   integrationMode: "embed",
             // },
+            customization: {
+              anonymous: {
+                request: true,
+                label: "Guest",
+              },
+              autosave: false,
+              comments: true,
+              compactHeader: false,
+              compactToolbar: false,
+              compatibleFeatures: false,
+              forcesave: false,
+              help: true,
+              hideRightMenu: true,
+              hideRulers: false,
+              integrationMode: "embed",
+              macros: true,
+              macrosMode: "Warn",
+              mentionShare: true,
+              mobileForceView: true,
+              plugins: true,
+              toolbarHideFileName: true,
+              toolbarNoTabs: true,
+              uiTheme: "theme-dark",
+              unit: "cm",
+              zoom: 100,
+            },
           },
         }}
         documentServerUrl={documentServerUrl}
